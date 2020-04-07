8ae059be391aa88074a898c31a96697a0bdd0ad5
ufoo68 <mitarou.yuutarou@gmail.com>
2020-04-07 19:05:23 +0900

    Apply waf

--- lib/aws-cdk-deploy-react-stack.ts
+++ lib/aws-cdk-deploy-react-stack.ts
@@ -3,21 +3,61 @@ import * as cloudfront from '@aws-cdk/aws-cloudfront'
 import * as s3 from '@aws-cdk/aws-s3'
 import * as s3deploy from '@aws-cdk/aws-s3-deployment'
 import * as iam from '@aws-cdk/aws-iam'
- 
+import * as waf from "@aws-cdk/aws-waf"
+
 export class AwsCdkDeployReactStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
- 
+    super(scope, id, props)
+
+    const ipSet = new waf.CfnIPSet(this, 'IpSet', {
+      name: 'whiteList',
+      ipSetDescriptors: [
+        {
+          type: 'IPV4',
+          value: '0.0.0.0/32'
+        }
+      ]
+    })
+
+    const rule = new waf.CfnRule(this, 'rule', {
+      metricName: 'whiteListRule',
+      name: 'whiteList',
+      predicates: [
+        {
+          dataId: ipSet.ref,
+          negated: false,
+          type: 'IPMatch',
+        }
+      ]
+    })
+
+    const webAcl = new waf.CfnWebACL(this, 'webAcl', {
+      defaultAction: {
+        type: 'BLOCK',
+      },
+      metricName: 'webAcl',
+      name: 'webAcl',
+      rules: [
+        {
+          action: {
+            type: 'ALLOW',
+          },
+          priority: 1,
+          ruleId: rule.ref,
+        }
+      ]
+    })
+
     const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
       websiteErrorDocument: 'index.html',
       websiteIndexDocument: 'index.html',
-    });
- 
+    })
+
     const websiteIdentity = new cloudfront.OriginAccessIdentity(
       this,
       'WebsiteIdentity',
-    );
- 
+    )
+
     const webSiteBucketPolicyStatement = new iam.PolicyStatement({
       actions: ['s3:GetObject'],
       effect: iam.Effect.ALLOW,
@@ -25,28 +65,12 @@ export class AwsCdkDeployReactStack extends cdk.Stack {
         websiteIdentity.grantPrincipal,
       ],
       resources: [`${websiteBucket.bucketArn}/*`],
-    });
- 
-    websiteBucket.addToResourcePolicy(webSiteBucketPolicyStatement);
- 
+    })
+    websiteBucket.addToResourcePolicy(webSiteBucketPolicyStatement)
     const websiteDistribution = new cloudfront.CloudFrontWebDistribution(
       this,
       'WebsiteDistribution',
       {
-        errorConfigurations: [
-          {
-            errorCachingMinTtl: 0,
-            errorCode: 403,
-            responseCode: 200,
-            responsePagePath: '/index.html',
-          },
-          {
-            errorCachingMinTtl: 0,
-            errorCode: 404,
-            responseCode: 200,
-            responsePagePath: '/index.html',
-          },
-        ],
         originConfigs: [
           {
             s3OriginSource: {
@@ -60,15 +84,14 @@ export class AwsCdkDeployReactStack extends cdk.Stack {
             ],
           },
         ],
-        priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
+        webACLId: webAcl.ref,
       },
-    );
- 
+    )
     new s3deploy.BucketDeployment(this, 'WebsiteDeploy', {
       sources: [s3deploy.Source.asset('./react-liff/build')],
       destinationBucket: websiteBucket,
       distribution: websiteDistribution,
       distributionPaths: ['/*'],
-    });
+    })
   }
 }
